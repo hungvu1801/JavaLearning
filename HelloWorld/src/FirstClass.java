b5dd6236c698d123275f6d8c27a099836478cac9
hungvt <vutienhung2212@gmail.com>
2023-08-28 09:42:06 +0700

    First time commit

---
+++ HelloWorld/src/FirstClass.java
@@ -0,0 +1,18 @@
+public class FirstClass {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    
+    boolean isAlien = false;
+    if (isAlien == false) {
+      System.out.print("It is not an alien");
+    }
+
+    int topScore = 100;
+    if (topScore == 100) {
+      System.out.println("You got the highest score.");
+    }
+
+  }
+  
+}
---
+++ HelloWorld/src/SecondClass.java
@@ -0,0 +1,7 @@
+public class SecondClass {
+  public static void main(String[] args) {
+    char fst = 'Q';
+    char scd = 'A';
+    System.out.print("" + fst + scd + "\n");
+  }
+}
