f9eeabae83abc3fb123531a0cb909a8aa9f9d570
Mike (Hung) <vutienhung2212@gmail.com>
2024-01-05 16:58:18 +0700

    Add serveral files

---
+++ CodeExercise/src/Point.java
@@ -0,0 +1,33 @@
+// CLASS PART 1
+public class Point {
+    private int x;
+    private int y;
+    public Point() {
+    }
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public double distance () {
+        return (double) Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+    public double distance (int x, int y) {
+        return (double) Math.sqrt((this.x - x) * (this.x - x) + (this.y - y) * (this.y - y));
+    }
+    public double distance (Point that) {
+        return (double) Math.sqrt((this.x - that.x) * (this.x - that.x) + (this.y - that.y) * (this.y - that.y));
+    }
+
+}
